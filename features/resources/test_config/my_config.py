d77bb10710bd38d17682a49376328b0b38217537
Umang Bhatia <umangbhatia786@gmail.com>
2022-05-28 16:26:16 +0530

    removed API Key from config

--- features/resources/test_config/my_config.py
+++ features/resources/test_config/my_config.py
@@ -1,4 +1,4 @@
-APPLITOOLS_API_KEY = 'sQ95aLWFirE78wrFQUzsk103zwgJYbU7oIOrelVQcD35A110'
+APPLITOOLS_API_KEY = '<Your API Key Here>'
 BROWSER = 'CHROME'
 APP_NAME = 'MY_APP'
 APP_UNDER_TEST = 'https://automationbookstore.dev/'
